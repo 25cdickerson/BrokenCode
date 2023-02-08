975cc8d3c7ff5027e03dbe83a0cbbb77a4a04415
Asa Smith <asasmith@Asas-MacBook-Air.local>
2023-02-07 23:26:46 -0500

    test cases finish

--- src/test/java/employee/PayRollTest.java
+++ src/test/java/employee/PayRollTest.java
@@ -1,28 +1,42 @@
 package employee;
 
 
-import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
 
 public class PayRollTest {
-    public static void main(String[] args) {
+    @Test
+    public void testAdd() {
         PayRoll payroll = new PayRoll();
+        HourlyEmp hourlyEmp = new HourlyEmp("J", 15, 40);
+        SalEmp salEmp = new SalEmp("A", 2000);
+        Contract contract = new Contract("K", 25, 20);
 
-        HourlyEmp hourlyEmp1 = new HourlyEmp("Hourly Emp 1", 10, 45);
-        HourlyEmp hourlyEmp2 = new HourlyEmp("Hourly Emp 2", 8, 35);
+        payroll.add(hourlyEmp);
+        payroll.add(salEmp);
+        payroll.add(contract);
 
-        Contract contractEmp1 = new Contract("Contract Emp 1", 15, 25);
-        Contract contractEmp2 = new Contract("Contract Emp 2", 20, 30);
+        ArrayList<employ> employees = payroll.employees;
+        assertArrayEquals(new employ[]{hourlyEmp, salEmp, contract}, employees.toArray());
+    }
 
-        SalEmp salEmp1 = new SalEmp("Salary Emp 1", 4000);
-        SalEmp salEmp2 = new SalEmp("Salary Emp 2", 6000);
+    @Test
+    public void testPayMethod() {
+        PayRoll payroll = new PayRoll();
+        HourlyEmp hourlyEmp = new HourlyEmp("J", 15, 40);
+        SalEmp salEmp = new SalEmp("A", 2000);
+        Contract contract = new Contract("K", 25, 20);
 
-        payroll.add(hourlyEmp1);
-        payroll.add(hourlyEmp2);
-        payroll.add(contractEmp1);
-        payroll.add(contractEmp2);
-        payroll.add(salEmp1);
-        payroll.add(salEmp2);
+        payroll.add(hourlyEmp);
+        payroll.add(salEmp);
+        payroll.add(contract);
 
         payroll.pay();
+        // expected output: [600.0, 800.0, 500.0]
     }
+
+   
 }
