5637b2242e6725d7012d693fd010367c97ddf227
Christopher (Preston) Dickerson <73359174+25cdickerson@users.noreply.github.com>
2023-02-14 21:10:16 -0500

    Broken Code

--- src/test/java/employee/PayRollTest.java
+++ src/test/java/employee/PayRollTest.java
@@ -10,32 +10,48 @@ import org.junit.jupiter.api.Test;
 public class PayRollTest {
     @Test
     public void testAdd() {
+    	// Initialize employees
         PayRoll payroll = new PayRoll();
         HourlyEmp hourlyEmp = new HourlyEmp("J", 15, 40);
         SalEmp salEmp = new SalEmp("A", 2000);
         Contract contract = new Contract("K", 25, 20);
 
+        // Add employees to payroll
         payroll.add(hourlyEmp);
         payroll.add(salEmp);
         payroll.add(contract);
-
-        ArrayList<employ> employees = payroll.employees;
+        
+        // Checks array
+        ArrayList<employ> employees = payroll.l;
         assertArrayEquals(new employ[]{hourlyEmp, salEmp, contract}, employees.toArray());
     }
 
     @Test
     public void testPayMethod() {
+    	// Initialize employees
         PayRoll payroll = new PayRoll();
         HourlyEmp hourlyEmp = new HourlyEmp("J", 15, 40);
         SalEmp salEmp = new SalEmp("A", 2000);
         Contract contract = new Contract("K", 25, 20);
 
+        // Add employees to payroll
         payroll.add(hourlyEmp);
         payroll.add(salEmp);
         payroll.add(contract);
+        
+        // Check getPay works
+        assert salEmp.getPay() == 80000.0;
+        
+        // Store payroll.pay
+        double payEmp[] = payroll.pay();
+        
 
-        payroll.pay();
-        // expected output: [600.0, 800.0, 500.0]
+        // Test payroll.pay
+        double[] test = {600.0, 80000.0, 500.0};
+        for(int i = 0; i < 2; i++) {
+        		assertEquals(payEmp[i], test[i]);
+        }
+        // expected output: [600.0, 80000.0, 500.0]
     }
 
    
