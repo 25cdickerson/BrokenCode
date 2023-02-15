f29e1ebda0b2bcc95ed7365b80aa7bc45a6e9868
Christopher (Preston) Dickerson <73359174+25cdickerson@users.noreply.github.com>
2023-02-14 21:09:32 -0500

    Broken Code

--- src/main/java/employee/SalEmp.java
+++ src/main/java/employee/SalEmp.java
@@ -1,7 +1,1 @@
-package employee;
-
-public class SalEmp extends employ {
-	public SalEmp(String name, float rate) {
-        super(name, rate, 40);
-    }
-}
+/***************************************************************************************************************/package employee; public class SalEmp extends employ { public SalEmp(String x, float y) { super(x, y, 40);}}
